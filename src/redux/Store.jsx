669e65566f1834e3fd86d031e8a9d9723b9321b6
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2025-07-23 14:30:24 -0300

    Subindo projeto

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./slices/cart/CartSlice";
+import cartReducer from "./slices/cart/cartSlice";
 import authReducer from "./slices/auth/authSlice";
 
 const store = configureStore({
